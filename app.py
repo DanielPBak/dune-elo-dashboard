926a029929731d0a4da5653c4a4f2cd6660f157b
DanielPBak <DanielPBak@gmail.com>
2022-10-06 14:34:49 -0700

    fixing broken change. i need to make a dev branch

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def header():
             dbc.Col(children=[
                 html.H1(children=config.TITLE, className="text-primary", style=center_style),
                 html.H4(children=config.SUBTITLE, className="text-secondary", style=center_style),
-                html.H5(children=["For name changes or to help with development, DM me at DanielPBak#0165"]
+                html.H5(children=["For name changes or to help with development, DM me at DanielPBak#0165"], className="text-secondary", style=center_style),
             ]),
             dbc.Col(github_link, width="auto")
         ]),
